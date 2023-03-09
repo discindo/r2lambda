7e9a92ce3ebb8a4960f905e275df5917282dbc4d
Teofil <teofiln@gmail.com>
2023-03-09 10:01:47 -0600

    doc: update manual

--- R/s3.R
+++ R/s3.R
@@ -1,4 +1,7 @@
-#' update policy template with target bucket
+#' Update policy template with target bucket
+#' Not used at this time
+#' @param bucket_arn the ARN of an S3 bucket
+#' @noRd
 prepare_s3_put_policy <- function(bucket_arn) {
   role <- system.file("lambda-S3-put-role.json", package = "r2lambda")
   role_list <- jsonlite::fromJSON(role)
@@ -6,6 +9,3 @@ prepare_s3_put_policy <- function(bucket_arn) {
   role_string <- lambdr::as_stringified_json(role_list)
   return(role_string)
 }
-
-
-# deploy_lambda(tag = "tidytuesday3")
